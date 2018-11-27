a58dd68ef48da29202ae46f34be757629f85f871
Dylan Verheul <dylan@zostera.nl>
2018-11-27 05:42:20 +0100

    Enable admin for Attribute, Choice, Unit

--- attrs/admin.py
+++ attrs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from attrs.models import Attribute, Choice, Unit
+
+admin.site.register(Attribute)
+admin.site.register(Unit)
+admin.site.register(Choice)
